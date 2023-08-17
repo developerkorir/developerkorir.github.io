93c8dcc6c65c9323ea87d1b9b04e5219b001e846
developerkorir <caltonkorir45@gmail.com>
2023-08-17 23:47:25 +0300

    Construction, blog introduced

--- script.js
+++ script.js
@@ -48,4 +48,14 @@ window.addEventListener('scroll', function() {
     animate();
     });
 	}
+
+  $('.post-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
+  });
+
 });
